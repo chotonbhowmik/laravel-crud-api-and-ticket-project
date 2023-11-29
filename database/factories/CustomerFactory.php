844aba11b53cbb52aba7976f25a5c394cc778b0d
Choton Chandra Bhowmik <83583216+chotonbhowmik@users.noreply.github.com>
2023-11-29 11:22:58 +0600

    finish seeder and factories for customer and invoice

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Factories;
-
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +9,19 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CustomerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+   
     public function definition(): array
     {
+        $type = $this->faker->randomElement(['I', 'B']); // Use randomElement instead of randomElements
+        $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
         return [
-            //
+            'name' => $name,
+            'type'=> $type,
+            'email' => $this->faker->email(),
+            'address'=> $this->faker->streetAddress(),
+            'city'=> $this->faker->city(),
+            'state'=> $this->faker->state(),
+            'postal_code'=> $this->faker->postcode(),
         ];
     }
 }
--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Factories;
-
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +9,16 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class InvoiceFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition(): array
     {
+        $status = $this->faker->randomElement(['B','P','V']);
         return [
-            //
+           'customer_id' => Customer::factory(),
+           'amount' => $this->faker->numberBetween(100,20000),
+           'status'=> $status,
+           'billed_date'=> $this->faker->dateTimeThisDecade(),
+           'paid_date'=> $status == 'P' ? $this->faker->dateTimeThisCentury() : NULL,
         ];
     }
 }
